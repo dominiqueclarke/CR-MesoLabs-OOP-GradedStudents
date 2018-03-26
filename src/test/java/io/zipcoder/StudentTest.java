cbd72770cb7c794cf713598492773afddf8bcfdf
Dominique Clarke <dominiquedclarke@gmail.com>
2018-03-26 16:49:58 -0400

    organized tests

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -8,98 +8,134 @@ public class StudentTest {
 
     @Test
     public void testGetFirstName() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         String expected = "Dominique";
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         String actual = student.getFirstName();
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGetLastName() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         String expected = "Clarke";
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         String actual = student.getLastName();
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGetNumberOfExams() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         int expected = 3;
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         int actual = student.getNumberOfExams();
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGetExamScores() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         String expected = "Exam Scores:\n" +
                 "\tExam 1 -> 100\n" +
                 "\tExam 2 -> 99\n" +
                 "\tExam 3 -> 98";
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         String actual = student.getExamScores();
         assertEquals(expected, actual);
     }
 
     @Test
     public void testAddExamScore1() {
-        testScores = new Double[0];
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
+        Double[] testScores = new Double[0];
         String expected = "Exam Scores:\n" +
                 "\tExam 1 -> 100";
+
+        // When
+        Student student = new Student("Dominique", "Clarke", testScores);
+
+        // Then
         String actual = student.addExamScore(100.0);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testAddExamScore2() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         String expected = "Exam Scores:\n" +
                 "\tExam 1 -> 100\n" +
                 "\tExam 2 -> 99\n" +
                 "\tExam 3 -> 98\n" +
                 "\tExam 4 -> 100";
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         String actual = student.addExamScore(100.0);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testSetExamScore() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         String expected = "Exam Scores:\n" +
                 "\tExam 1 -> 100\n" +
                 "\tExam 2 -> 99\n" +
                 "\tExam 3 -> 100";
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         String actual = student.setExamScore(3, 100.0);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGetAverageExamScore() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         double expected = 99;
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         double actual = student.getAverageExamScore();
         assertEquals(expected, actual, .01);
     }
 
     @Test
     public void testToString() {
-        Student student = new Student("Dominique", "Clarke", testScores);
-
+        // Given
         String expected = "Student Name: Dominique Clarke\n" +
                 "> Average Score: 99\n" +
                 "> Exam Scores:\n" +
                 "\tExam 1 -> 100\n" +
                 "\tExam 2 -> 99\n" +
                 "\tExam 3 -> 98";
+
+        // When
+        Student student = new Student("Dominique", "Clarke", this.testScores);
+
+        // Then
         String actual = student.toString();
         assertEquals(expected, actual);
     }
